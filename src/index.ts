d43f36e168c8e8b252b306c3d8dc3f0627ef8bf6
Thinh <nguyenhungthinh2504@gmail.com>
2024-05-19 17:30:32 +0700

    set up DB

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,12 @@ import cors from "cors";
 import "dotenv/config";
 import express, { Request, Response } from "express";
 
+import mongoose from "mongoose";
+mongoose
+  .connect(process.env.MONGODB_URI as string)
+  .then(() => console.log("Connect Database Successfully!"))
+  .catch((err) => console.log(err));
+
 const app = express();
 app.use(express.json());
 app.use(cors());
